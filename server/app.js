2a72d3779b7860bff148dcb9ab50bce3e4969351
Jonathan Ly <jonathanlyhv@gmail.com>
2017-06-16 18:42:54 +1000

    Removed unneccesary dependancies

--- server/app.js
+++ server/app.js
@@ -8,10 +8,6 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
